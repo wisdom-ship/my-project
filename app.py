97aa70c733a29e2c1d6d904d49239eeec2201960
wisdom-ship <mugerwadenis53@gmail.com>
2024-10-27 13:49:54 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__Coin TradeWave#__)
+
+@app.route('/')
+def home():
+    return "Welcome to Coin TradeWave!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
